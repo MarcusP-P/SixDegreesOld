f48eedcccf40adc68fd34a0f98c248251d87d971
Marcus Pallinger <marcus@pallinger.org>
2019-08-23 18:09:39 +1000

    Fix warnings
    Move the WeatherForcast class form a nested class

--- SixDegrees/Controllers/SampleDataController.cs
+++ SixDegrees/Controllers/SampleDataController.cs
@@ -1,3 +1,4 @@
+using SixDegrees.Model;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
@@ -29,19 +30,5 @@ namespace SixDegrees.Controllers
             });
         }
 
-        public class WeatherForecast
-        {
-            public string DateFormatted { get; set; }
-            public int TemperatureC { get; set; }
-            public string Summary { get; set; }
-
-            public int TemperatureF
-            {
-                get
-                {
-                    return 32 + (int)(TemperatureC / 0.5556);
-                }
-            }
-        }
     }
 }
