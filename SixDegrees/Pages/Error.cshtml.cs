6bfe34fcc2c7637af175da4f6428e305d03b47a8
Marcus Pallinger <marcus@pallinger.org>
2019-08-27 20:50:31 +1000

    Fix warning
    Warning SA1649  File name should match first type name

--- SixDegrees/Pages/Error.cshtml.cs
+++ SixDegrees/Pages/Error.cshtml.cs
@@ -13,7 +13,9 @@ namespace SixDegrees.Pages
     /// ViewModel for the error page.
     /// </summary>
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+#pragma warning disable SA1649 // File name should match first type name
     public class ErrorModel : PageModel
+#pragma warning restore SA1649 // File name should match first type name
     {
         /// <summary>
         /// Gets or sets the RequestID.
