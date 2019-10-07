9ed3b0f30cf114c85de51adf43b3e4556079d37d
Marcus Pallinger <marcus@pallinger.org>
2019-10-08 00:15:07 +1100

    Ad eslint-plugin-angular
    Move eslint to devDependencies and

--- SixDegrees/ClientApp/src/main.ts
+++ SixDegrees/ClientApp/src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 export function getBaseUrl() {
-  return document.getElementsByTagName('base')[0].href;
+  return document.getElementsByTagName('base')[0].href;  // eslint-disable-line angular/document-service
 }
 
 const providers = [
